4767c6beac256e6c5d1e30e794b177e36ae76904
夏玮 <1006903445@qq.com>
2023-06-23 16:50:09 +0800

    commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <h1>hell world</h1>
+</template>
+
+<style scoped lang="sass">
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@/src/theme/style.sass";`
+      }
+    }
+  },
+  server: {
+    port: 3000
+  }
+});
